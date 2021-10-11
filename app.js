1df590f03c5e1f84e3d964dfdf252ac1b4d714af
Pierre Charnacé <ervan_St@hotmail.fr>
2021-10-11 16:51:48 +0200

    bodyParse

--- app.js
+++ app.js
@@ -1,5 +1,73 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-module.export = app;
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use('/api/auth/signup',(req, res, next) => {
+    console.log('requête reçue !');
+    res.status(200).json('Vous êtes bien enregistré');
+    next();
+});
+
+app.post('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'sauce créée !'
+    })
+})
+
+app.use('/api/sauces', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+    next();
+});
+
+app.use('/api/auth/login',(req,res, next) => {
+    res.status(201);
+     res.json({message: 'Votre requête a bien été reçue !'})
+    next();
+});
+
+app.use('/api/sauces',(req, res, next) => {
+   
+    next();
+})
+
+app.use('/api/sauces/:id',(req, res, next) => {
+    console.log('réponse finale');
+
+    next();
+});
+
+app.use('/api/sauces',(req, res, next) => {
+    console.log('réponse finale');
+});
+
+
+module.exports = app;
