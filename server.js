83ac7349a4f41305b6e735c70da3ddace31795d3
Pierre Charnacé <ervan_St@hotmail.fr>
2021-10-11 11:19:39 +0200

    express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end ('voilà la réponse du serveur !');
+    res.end ('voilà la réponse du premier serveur !');
 });
 
 server.listen(process.env.PORT || 3000);
